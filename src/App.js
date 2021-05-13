db7638453d27fcb65beda73ec52b723bec19b42c
Taro Seo <mail2017@seotaro.com>
2021-05-13 18:19:10 +0900

    refactoring

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import DeckGL from '@deck.gl/react';
 import { MapView } from '@deck.gl/core';
 import { GeoJsonLayer, SolidPolygonLayer, ScatterplotLayer, ColumnLayer, GridCellLayer } from '@deck.gl/layers';
 import { GridLayer, HeatmapLayer, HexagonLayer } from '@deck.gl/aggregation-layers';
-import fetch from 'node-fetch';
-import moment from 'moment';
-import { normalize, mix, hsvToRgb, } from './utils'
-
-const TABLE_URL = 'https://www.jma.go.jp/bosai/amedas/const/amedastable.json';
-const LATEST_TIME_URL = 'https://www.jma.go.jp/bosai/amedas/data/latest_time.txt';
-const DATA_URL = date => `https://www.jma.go.jp/bosai/amedas/data/map/${moment(date).format("YYYYMMDDHHmmss")}.json`;
+import { normalize, mix, hsvToRgb, getAmedasLatestTime, getAmedas } from './utils'
 
 const INITIAL_VIEW_STATE = {
   longitude: 136.0,
@@ -40,53 +34,8 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      const latestTime = await fetch(LATEST_TIME_URL)
-        .then(response => {
-          return response.text();
-        })
-        .then(text => {
-          return new Date(text);
-        })
-        .catch((error) => {
-          console.error(error)
-        });
-
-      const urls = [TABLE_URL, DATA_URL(latestTime)];
-      const [table, data] = await Promise.all(urls.map((url) => {
-        return fetch(url)
-          .then(response => {
-            return response.text();
-          })
-          .then(text => {
-            return JSON.parse(text);
-          })
-          .catch((error) => {
-            console.error(error)
-          })
-      }))
-
-      const stations = Object.keys(data).map(k => {
-        const t = table[k];
-        const d = data[k];
-        const station = {
-          code: k,
-          name: t.kjName,
-          coordinates: [t.lon[0] + t.lon[1] / 60.0, t.lat[0] + t.lat[1] / 60.0],
-          precipitation10m: d.precipitation10m && (d.precipitation10m[1] === 0) ? d.precipitation10m[0] : null,
-          precipitation1h: d.precipitation1h && (d.precipitation1h[1] === 0) ? d.precipitation1h[0] : null,
-          precipitation3h: d.precipitation3h && (d.precipitation3h[1] === 0) ? d.precipitation3h[0] : null,
-          precipitation24h: d.precipitation24h && (d.precipitation24h[1] === 0) ? d.precipitation24h[0] : null,
-          wind: d.wind && d.windDirection && (d.wind[1] === 0) && (d.windDirection[1] === 0) ? [d.wind[0], d.windDirection[0]] : null,
-          temp: d.temp && (d.temp[1] === 0) ? d.temp[0] : null,
-          sun1h: d.sun1h && (d.sun1h[1] === 0) ? d.sun1h[0] : null,
-          snow: d.snow && (d.snow[1] === 0) ? d.snow[0] : null,
-          snow6h: d.snow6h && (d.snow6h[1] === 0) ? d.snow6h[0] : null,
-          snow12h: d.snow12h && (d.snow12h[1] === 0) ? d.snow12h[0] : null,
-          snow24h: d.snow24h && (d.snow24h[1] === 0) ? d.snow24h[0] : null,
-          humidity: d.humidity && (d.humidity[1] === 0) ? d.humidity[0] : null,
-        };
-        return station;
-      });
+      const latestTime = await getAmedasLatestTime();
+      const stations = await getAmedas(latestTime);
 
       const layer = (function (element) {
         const values = stations.map(x => {
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
+import fetch from 'node-fetch';
+import moment from 'moment';
+
+const TABLE_URL = 'https://www.jma.go.jp/bosai/amedas/const/amedastable.json';
+const LATEST_TIME_URL = 'https://www.jma.go.jp/bosai/amedas/data/latest_time.txt';
+const DATA_URL = date => `https://www.jma.go.jp/bosai/amedas/data/map/${moment(date).format("YYYYMMDDHHmmss")}.json`;
 
 export const normalize = (value, min, max) => ((value - min) / (max - min));
 export const mix = (value, min, max) => (min * (1.0 - value) + max * value);
@@ -25,4 +31,56 @@ export function hsvToRgb(H, S, V) {
     }
 
     return rgb.map(x => Math.floor(x * 255)); // 0 〜 255
+}
+
+export async function getAmedasLatestTime() {
+    return fetch(LATEST_TIME_URL)
+        .then(response => {
+            return response.text();
+        })
+        .then(text => {
+            return new Date(text);
+        })
+        .catch((error) => {
+            console.error(error)
+        });
+}
+
+export async function getAmedas(date) {
+    const urls = [TABLE_URL, DATA_URL(date)];
+    const [table, data] = await Promise.all(urls.map((url) => {
+        return fetch(url)
+            .then(response => {
+                return response.text();
+            })
+            .then(text => {
+                return JSON.parse(text);
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }))
+
+    return Object.keys(data).map(k => {
+        const t = table[k];
+        const d = data[k];
+        const station = {
+            code: k,
+            name: t.kjName,
+            coordinates: [t.lon[0] + t.lon[1] / 60.0, t.lat[0] + t.lat[1] / 60.0],
+            precipitation10m: d.precipitation10m && (d.precipitation10m[1] === 0) ? d.precipitation10m[0] : null,
+            precipitation1h: d.precipitation1h && (d.precipitation1h[1] === 0) ? d.precipitation1h[0] : null,
+            precipitation3h: d.precipitation3h && (d.precipitation3h[1] === 0) ? d.precipitation3h[0] : null,
+            precipitation24h: d.precipitation24h && (d.precipitation24h[1] === 0) ? d.precipitation24h[0] : null,
+            wind: d.wind && d.windDirection && (d.wind[1] === 0) && (d.windDirection[1] === 0) ? [d.wind[0], d.windDirection[0]] : null,
+            temp: d.temp && (d.temp[1] === 0) ? d.temp[0] : null,
+            sun1h: d.sun1h && (d.sun1h[1] === 0) ? d.sun1h[0] : null,
+            snow: d.snow && (d.snow[1] === 0) ? d.snow[0] : null,
+            snow6h: d.snow6h && (d.snow6h[1] === 0) ? d.snow6h[0] : null,
+            snow12h: d.snow12h && (d.snow12h[1] === 0) ? d.snow12h[0] : null,
+            snow24h: d.snow24h && (d.snow24h[1] === 0) ? d.snow24h[0] : null,
+            humidity: d.humidity && (d.humidity[1] === 0) ? d.humidity[0] : null,
+        };
+        return station;
+    });
 }
