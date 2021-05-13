97e0b52b23024ba030cf5adb092b650ccecf25ac
Taro Seo <mail2017@seotaro.com>
2021-05-13 18:08:56 +0900

    color

--- src/App.js
+++ src/App.js
@@ -20,6 +20,50 @@ const INITIAL_VIEW_STATE = {
   bearing: 0
 };
 
+const normalize = (value, min, max) => ((value - min) / (max - min));
+const mix = (value, min, max) => (min * (1.0 - value) + max * value);
+
+function hsvToRgb(H, S, V) {
+  if (360.0 <= H) {
+    H = 0.0;
+  }
+
+  const Hi = Math.floor(H / 60.0) % 6;
+  const f = H / 60.0 - Hi;
+
+  const p = V * (1.0 - S);
+  const q = V * (1.0 - S * f);
+  const t = V * (1.0 - S * (1.0 - f));
+
+  let rgb = [0.0, 0.0, 0.0]; // 0.0 〜 1.0
+  switch (Hi) {
+    case 0: rgb = [V, t, p]; break;
+    case 1: rgb = [q, V, p]; break;
+    case 2: rgb = [p, V, t]; break;
+    case 3: rgb = [p, q, V]; break;
+    case 4: rgb = [t, p, V]; break;
+    case 5: rgb = [V, p, q]; break;
+  }
+
+  return rgb.map(x => Math.floor(x * 255)); // 0 〜 255
+}
+
+
+const settings = {
+  'precipitation10m': { name: "10分間降水量", min: 0.0, max: 100.0, offset: 0.0 },
+  'precipitation1h': { name: "1時間降水量", min: 0.0, max: 100.0, offset: 0.0 },
+  'precipitation3h': { name: "3時間降水量", min: 0.0, max: 100.0, offset: 0.0 },
+  'precipitation24h': { name: "24時間降水量", min: 0.0, max: 100.0, offset: 0.0 },
+  'wind': { name: "風向・風速", min: 0.0, max: 100.0, offset: 0.0 },
+  'temp': { name: "気温", min: -10.0, max: 30.0, offset: 0.0, colorFunc: ((value) => hsvToRgb(mix(value, 240.0, 0.0), 1.0, 1.0)) },
+  'sun1h': { name: "日照時間", min: 0.0, max: 100.0, offset: 0.0 },
+  'snow': { name: "積雪深", min: 0.0, max: 100.0, offset: 0.0 },
+  'snow6h': { name: "6時間降雪量", min: 0.0, max: 100.0, offset: 0.0 },
+  'snow12h': { name: "12時間降雪量", min: 0.0, max: 100.0, offset: 0.0 },
+  'snow24h': { name: "24時間降雪量", min: 0.0, max: 100.0, offset: 0.0 },
+  'humidity': { name: "湿度", min: 0.0, max: 100.0, offset: 0.0, colorFunc: ((value) => hsvToRgb(mix(value, 240.0, 0.0), 1.0, 1.0)) },
+};
+
 function App() {
   const [layer, setLayer] = useState(null);
 
@@ -71,22 +115,33 @@ function App() {
           humidity: d.humidity && (d.humidity[1] === 0) ? d.humidity[0] : null,
         };
         return station;
-      }
-      );
+      });
 
       const layer = (function (element) {
+        const values = stations.map(x => {
+          const normlizedValue = x[element] ? normalize(x[element], settings[element].min, settings[element].max) : 0.0;
+          return ({
+            code: x.code,
+            name: x.name,
+            coordinates: x.coordinates,
+            normlizedValue: normlizedValue,
+            value: x[element],  // tooltip で出力する。
+            color: x[element] ? settings[element].colorFunc(normlizedValue) : [0, 0, 0, 32],
+          })
+        });
+
         return (<GridCellLayer
           id={'gridcelllayer'}
-          data={stations}
+          data={values}
           pickable={true}
           cellSize={5000}
           extruded={true}
-          elevationScale={1000}
+          elevationScale={50000}
           getPosition={d => d.coordinates}
-          getFillColor={d => d[element] ? [48, 128, 255, 255] : [0, 0, 0, 32]}
-          getElevation={d => d[element] ? d[element] : 0}
+          getFillColor={d => d.color}
+          getElevation={d => d.normlizedValue}
         />);
-      }('humidity'));
+      }('temp'));
 
       setLayer(layer);
     })();
